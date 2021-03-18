0c0d21a4f1ec58f0001f176c3ffa07d67db2ada6
abeldemiss <69866709+abeldemiss@users.noreply.github.com>
2021-03-17 22:39:14 -0400

    script

--- bg-color-changer/script.js
+++ bg-color-changer/script.js
@@ -10,10 +10,10 @@ const colors = [
     'brown'
 ];
 
-body.style.backgroundColor = 'steelblue';
-button.addEventListener('click', randomColor);
+body.style.backgroundColor = 'steelblue'; //set initial background color
+button.addEventListener('click', randomColor); //on click executes function
 
 function randomColor(){
-    const changeColor = parseInt(Math.random()*colors.length);
-    body.style.backgroundColor = colors[changeColor];
+    const changeColor = parseInt(Math.random()*colors.length); //chooses random string from array
+    body.style.backgroundColor = colors[changeColor]; //change bg color
 };
