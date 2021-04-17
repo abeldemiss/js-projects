74d36de009f6ff724069e0233ba18b86cb663ff3
abeldemiss <69866709+abeldemiss@users.noreply.github.com>
2021-04-17 16:34:02 -0400

    Number Guesser JS

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -3,8 +3,8 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-let generateTarget = () => {
-  return Math.floor(Math.random * 10);
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
 };
 
 const compareGuesses = (humanGuess, computerGuess, generateTarget) => {
@@ -18,4 +18,18 @@ const compareGuesses = (humanGuess, computerGuess, generateTarget) => {
   } else {
     return false;
   }
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  } else {
+    console.log("Error, enter human or computer.");
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
 };
